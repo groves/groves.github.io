ea664507b41446218958bac02f3a63b212e29b07
Charlie Groves <c@sevorg.org>
2022-10-13 13:17:47 -0400

    spellcheck before building and pushing

--- publish.sh
+++ publish.sh
@@ -3,6 +3,9 @@ set -o nounset
 SOURCE=~/dev/groves.github.io
 PAGES=~/dev/sevorg.org
 cd $SOURCE
+for f in content/posts/* ; do 
+  aspell check --dont-backup $f
+done
 hugo --buildDrafts --baseUrl https://sevorg.org/draft/ --destination $PAGES/draft/
 HUGO_ENV=production hugo --baseUrl https://sevorg.org/ --destination $PAGES
 cd $PAGES
